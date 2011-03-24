e546eec7913173df90834fb2e736c91d8f5586ce
airportyh <airportyh@gmail.com>
2011-03-24 11:49:57 -0400

    First commit for Indentinator!

---
+++ main.rb
@@ -0,0 +1,31 @@
+#! /usr/bin/env ruby
+require 'getopt/std'
+require 'indentor.rb'
+opts = Getopt::Std.getopts('c:vV')
+@to_amount = opts['c']
+@very_verbose = opts['V']
+@verbose = opts['v'] or @very_verbose
+@files = ARGV
+if @files.size == 0
+  puts "Usage: indent-amount <file>"
+  exit(0)
+end
+
+@files.each do |path|
+  File.open(path) do |file|
+    lines = file.readlines
+    amount = indent_amount(lines)
+    
+    if @to_amount.nil?
+      if amount.nil?
+        puts "Failed for #{path}. Is it binary?"
+      elsif amount == 0
+        puts "#{path} has no indentation."
+      else
+        puts "#{path} uses #{amount} spaces."
+      end
+    else
+      puts convert_lines(lines, amount, @to_amount).join("\n")
+    end
+  end
+end
---
+++ tests.rb
@@ -0,0 +1,66 @@
+require 'rspec'
+require 'indentor.rb'
+describe 'convert_file' do
+  it "converts basic indentation" do
+    text = "
+line 1
+  line 2
+    line 3
+line 4"
+    convert_indentation(text, 2, 4).should == "
+line 1
+    line 2
+        line 3
+line 4"
+  end
+    
+  it "retains non-standard indentation when not multiple 
+      of org indentation amount" do
+    text = "
+line 1
+   line 2"
+    convert_indentation(text, 2, 4).should == text
+  end
+  
+  it "retains non-standard indentation even when is 
+      multiple of org indentation amount" do
+    text = "
+line 1
+  line 2
+    line 3
+        line 2"
+    convert_indentation(text, 2, 4).should == "
+line 1
+    line 2
+        line 3
+            line 2"    
+  end
+  
+  it "retains line-up of non-standard indented lines" do
+    text = "
+line 1
+  line 2
+  line 3
+  line 4"
+    convert_indentation(text, 4, 2).should == "
+line 1
+  line 2
+  line 3
+  line 4"
+  end
+  
+  it "retains line-up of non-standard indented lines(ex 2)" do
+    text = "
+line 0
+    line 1
+      line 2
+      line 3
+      line 4"
+    convert_indentation(text, 4, 2).should == "
+line 0
+  line 1
+    line 2
+    line 3
+    line 4"
+  end
+end
